5fa74ebb875e48a49baa4442f2cac6712a0c32fb
Maxim Dobriakov <closer.main@gmail.com>
2010-05-23 23:02:18 +0400

    Implemented functionality for selection of page layout

--- template/app/controllers/application_controller.rb
+++ template/app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   TITLE = "Application Title"
+
+  attr_accessor_with_default :layout_type, :content_sidebar_layout
+  
 end
--- template/app/helpers/application_helper.rb
+++ template/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def layout_class
+    controller.layout_type.to_s.dasherize
+  end
+
+  def layout_partial
+    "layouts/partials/#{controller.layout_type}"
+  end
+  
 end
